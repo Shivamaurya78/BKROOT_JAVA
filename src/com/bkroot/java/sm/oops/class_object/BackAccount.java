f4b4d2038b93dbcd2e81b916736b6731387071fd
Shivamaurya78 <shivu89maurya@git config --global user.email>
2026-03-23 17:43:04 +0530

    sava

---
+++ src/com/bkroot/java/sm/oops/class_object/BackAccount.java
@@ -0,0 +1,25 @@
+package com.bkroot.corejava.basic.OOPS.ClassObject;
+
+public class BackAccount {
+    double balance;
+
+    void deposit(double amount)
+    {
+        balance +=amount;
+    }
+    void withdraw (double amount)
+    {
+        balance -=amount;
+    }
+    void display()
+    {
+        System.out.println("your balance is " +balance);
+    }
+
+    public static void main(String[] args) {
+        BackAccount ba = new BackAccount();
+        ba.deposit(5000);
+        ba.withdraw(3000);
+        ba.display();
+    }
+}
